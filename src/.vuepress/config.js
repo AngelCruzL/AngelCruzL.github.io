9e53d256b3a92aad256a9ee60464797e68cddc93
Ángel Cruz <langelcruzlara@gmail.com>
2020-12-26 00:02:43 -0600

    Fixing up sidebars

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: "Vuepress Docs Boilerplate",
+  title: "Ángel Cruz Notes",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -54,13 +54,9 @@ module.exports = {
       },
     ],
     sidebar: {
-      "/guide/": [
-        {
-          title: "Guide",
-          collapsable: false,
-          children: ["", "using-vue"],
-        },
-      ],
+      "/utilities/": [""],
+      "/frontend/": ["", "html", "css", "js"],
+      "/backend/": [""],
     },
   },
 
